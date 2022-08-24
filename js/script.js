76c85020196113aa8210fc8823f48aa933ef94fa
Your Name <anneserocki@gmail.comom>
2022-08-24 15:16:58 -0700

    commit07-01

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
+let addEmployeeForm = window.document.getElementById('addForm');
+let employeeTable = window.document.getElementById('employees');
+
 
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-
+let totalNumber
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
+    e.preventDefault();
 
     // GET THE VALUES FROM THE TEXT BOXES
+    let idVal = e.getElementById('id').value;
+
+    
 
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
 
